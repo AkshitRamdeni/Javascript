9a4f232b874b453bc295c7f2842f6fb7b0957349
AkshitRamdeni <108686558+AkshitRamdeni@users.noreply.github.com>
2022-07-04 22:45:47 +0530

    Add files via upload
    Java-Script

---
+++ script.js
@@ -0,0 +1,45 @@
+// let js = 'amazing';
+// // if (js === "amazing") alert("Java Script");
+
+// console.log(40 - 60 + 20 + 60);
+
+// // values and variable
+
+// console.log("Akshith");
+// console.log(20);
+
+// // Declearing variable 
+// let FirstName = 'Akshith';
+// console.log(FirstName);
+
+// let firstname = 'Programmer';
+// let MyCurrentJob = "Student";
+
+// let job1 = 'Student1';
+// let job2 = 'Programmer';
+
+// console.log(job2);
+
+let javascript = true;
+console.log(javascript);
+
+
+//typeof
+
+
+console.log(typeof(javascript));
+console.log(typeof(23));
+console.log(typeof("Akshith"));
+
+
+
+const now = 2022
+const ageRandi = now - 2001;
+const ageAndi = now - 2002;
+
+console.log(ageAndi, ageRandi);
+
+
+const Firstname = 'Akshith';
+const lastname = 'Ramdeni';
+console.log(Firstname + ' ' + lastname);
